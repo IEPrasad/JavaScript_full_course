784a54fa813131d8f59dae609beaef0a8efcca80
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2023-12-27 11:34:01 +0530

    Add files via upload

---
+++ exercises1.js
@@ -0,0 +1,27 @@
+// 1f. Use document.body.innerHTML = ---; to diplay
+  // 'Good morning!' on the web page.
+
+document.body.innerHTML = 'Good morning!'
+
+// 1g. Display your name on the web page.
+
+document.body.innerHTML = 'Isuru Eranda Prasad'
+
+// Challenge Exercises ---------
+
+// 1h. You order a T-shirt for $10, socks for $8, and dinner plates for 
+// $20. Use JavaScript to calculate the total cost your orsder.
+>> write in the console
+10 + 8 + 20
+
+// 1i. Your bank account has $100, you spend $20 on lunch,
+// $50 on dinner, and earn $200 from your job.
+// Calculate how much money you have
+
+100 - 20 - 50 + 200
+
+// 1j. use document.body.innerHTML = ...; to make the web page blank.
+
+document.body.innerHTML = '';
+
+
