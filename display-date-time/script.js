456dcc7915cc6c03c4df64331c7268d10174cdfe
IEPrasad <ieprasad23@gmail.com>
2025-09-18 00:12:29 +0530

    Deleted

--- display-date-time/script.js
+++
@@ -1,9 +0,0 @@
-function updateDateTime() {
-    const now = new Date();
-    const datetimeString = now.toLocaleString();
-    document.getElementById('datetime').textContent = datetimeString;
-}
-
-setInterval(updateDateTime, 1000); // Update the date and time every second
-
-window.onload = updateDateTime; // Initialize the date and time on page load
